b2df1c239a285e110dc315ef2a98caef9a19685f
Justus Spitzmüller <j.spitzmueller@posteo.de>
2021-11-08 23:41:27 +0100

    Made the converter class

--- saga_knipser.cpp
+++ saga_knipser.cpp
@@ -1,4 +1,63 @@
+class saga_knipser
+{
+public:
+	saga_knipser	(void)	
+	{
+		Create(0.0,1.0,0.0,1.0);
+	}
 
+	saga_knipser	( const CSG_Rect &Extent, bool bAspectRatio = false )
+	{
+		create( Extent, bAspectRation );
+	}
+
+	bool	create	( double x_offset, double x_scale, double, y_offset, double y_scale )
+	{
+		if( x_scale != 0.0 & yScale != 0.0 )
+		{
+			m_x_offset 	= x_offset;
+			m_x_scale 	= x_scale;
+			m_y_offset 	= y_offset;
+			m_y_scale 	= y_scale;
+
+			return true;
+		}
+
+		return false;
+	}
+
+	bool	create	(const CSG_Rect &Extent, bool bAspectRatio = false)
+	{
+	
+		double x_range	= Extent.Get_XRange();
+		double y_range 	= Extent.Get_YRange();
+		double x_min	= Extent.Get_XMin();
+		double y_min	= Extent.Get_YMin();
+
+		if( bAspectRatio )
+		{
+			if( x_ran < y_range )
+			{
+				x_range = y_range;
+			}
+			else if( y_range < x_range )
+			{
+				y_range = x_range;
+			}
+		}
+
+		return( x_range > 0 && y_range > 0 ? create(
+			x_min, (0x3FFFFFFFFFFFFFF) / x_range,
+			y_min, (0x3FFFFFFFFFFFFFF) / y_range
+		) : false );
+	}
+
+private:
+	double		m_x_offset 	= 0.0;
+	double		m_x_scale 	= 0.0;
+	double		m_y_offset 	= 0.0;
+	double		m_y_scale 	= 0.0;
+}
 
 bool saga_knipser::convert( CSG_Shapes *polygon_ptr, knipser::paths &clipper )
 {
